8cc6d147169ac131ba139c28b124942822a1be7e
Dyx1s <grafdonskiy@gmail.com>
2024-06-16 10:58:38 +0300

    added fetches and states

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,8 +7,16 @@ import MovieList from '../components/MovieList';
 
 
 const HomePage: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [page, setPage] = useState<number>(1);
 
-  
+  useEffect(() => {
+    fetchMovies(page).then(({ data: { docs } }) => {
+      setMovies(docs);
+    });
+  }, [page]);
+
+
   return (
     <Container className='home-container'>
       <MovieList />
