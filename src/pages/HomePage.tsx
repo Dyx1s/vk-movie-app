e1261f4ca2d2009e95b43df0a2e7ec7b9c2e1404
Dyx1s <grafdonskiy@gmail.com>
2024-06-18 12:26:09 +0300

    rewrite api.ts, start to do HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,44 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Typography, Pagination } from '@mui/material';
-/* import SearchBar from '../components/SearchBar';
-import Filter from '../components/Filter'; */
-import MovieList from '../components/MovieList';
-import { fetchMovies } from '../services/api';
-import { Movie, FilterOptions } from '../types/types';
-import '../styles/HomePage.css';
-
+import React from 'react'
+import '..styles/HomePage.css'
 const HomePage: React.FC = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [query, setQuery] = useState('');
-  const [filters, setFilters] = useState<FilterOptions>({ genres: [], rating: { kp: 0 }, year: 0 });
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
 
-  useEffect(() => {
-    const getMovies = async () => {
-      const data = await fetchMovies(page, query, filters.genres, filters.rating.kp, filters.year);
-      setMovies(data.docs);
-      setTotalPages(data.pages);
-    };
-    getMovies();
-  }, [query, filters, page]);
-
-  const handleFilterChange = (name: keyof FilterOptions, value: any) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [name]: value,
-    }));
-  };
 
   return (
-    <Container className="home-container">
-      <Typography variant="h2">Movie Database</Typography>
-      {/* <SearchBar query={query} onSearch={setQuery} />
-      <Filter filters={filters} onFilterChange={handleFilterChange} /> */}
-      <MovieList movies={movies} page={page} setPage={setPage}/>
+    <div>
       
-    </Container>
-  );
-};
+    </div>
+  )
+}
 
-export default HomePage;
+export default HomePage
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,36 +1,25 @@
 import axios from 'axios';
-import { ApiResponse } from '../types/types';
+import { ApiResponse, Movie } from '../types/types';
 
 const API_KEY = '7THRVK7-5Z0MB59-JPFESA4-XEBVCRM';
 const BASE_URL = 'https://api.kinopoisk.dev';
 
-export const fetchMovies = async (page: number, query: string, genres: { name: string }[], rating: number, year: number): Promise<ApiResponse> => {
-  try {
-    const response = await axios.get(`${BASE_URL}/v1/movie`, {
-      headers: {
-        'X-API-KEY': API_KEY,
-      },
-      params: {
-        page,
-        query,
-        genres: genres.map((genre) => genre.name).join(','),
-        /* rating: rating.toString(), */
-        /* year: year.toString(), */
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
-  }
-};
-
-export const fetchMovieDetails = (id: string) =>
-  axios.get(`${BASE_URL}/v1/movie/${id}`, {
-    headers: {
-      'X-API-KEY': API_KEY,
-    },
-  });
-
+export const fetchMoviesList = async(
+  page: number,
+  genres: { name: string }[],
+  ratings: number[],
+  year: number
+): Promise<ApiResponse> => {
   
+  const response = await axios.get<ApiResponse>(
+    ``
+  )
+  return response.data
+}
 
+export const fetchMovieById = async (id: string): Promise<Movie> => {
+  const response = await axios.get<Movie>(
+    ``
+  )
+  return response.data
+}
